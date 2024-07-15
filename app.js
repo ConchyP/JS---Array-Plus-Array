cabe9e59858d4c262e26b58ce3b875e62c5326af
Conchy <xConchyx@gmail.com>
2024-07-15 12:15:23 +0200

    test done

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 export function arrSum(arr1, arr2) {
-    // Inicializar las sumas a 0
-    var sumArr1 = 0;
-    var sumArr2 = 0;
 
-    // Sumar todos los elementos de la primera matriz
-    for (var i = 0; i < arr1.length; i++) {
+    let sumArr1 = 0;
+    let sumArr2 = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
         sumArr1 += arr1[i];
     }
 
-    // Sumar todos los elementos de la segunda matriz
-    for (var j = 0; j < arr2.length; j++) {
+    for (let j = 0; j < arr2.length; j++) {
         sumArr2 += arr2[j];
     }
 
-    // Devolver la suma de ambas sumas
     return sumArr1 + sumArr2;
 }
+
 
 
 console.log(arrSum([1, 2, 3], [4, 5, 6]));       // Output: 21
--- test/app.test.js
+++ test/app.test.js
@@ -2,29 +2,29 @@ import { describe, it, expect } from 'vitest';
 import { arrSum } from '../app.js';
 
 describe("arrSum", () => {
-    it("Should return the sum of the values ​​of arrays", () => {
+    it("Should return the sum of the values ​​of the two arrays.", () => {
         const arr1 = [1, 2, 3];  
         const arr2 = [4, 5, 6];
         expect(arrSum(arr1 , arr2)).toBe(21);
     });
 
-    it("Should return the sum of the values ​​of arrays", () => {
+    it("Should return the sum of the values ​​of the two arrays", () => {
         const arr1 = [-1, -2, -3];
         const arr2 = [-4, -5, -6];
         expect(arrSum(arr1 , arr2)).toBe(-21);
     });
 
-    it("Should return the sum of the values ​​of arrays", () => {
+    it("Should return the sum of the values ​​of the two arrays", () => {
         const arr1 = [0, 0, 0];
         const arr2 = [4, 5, 6];
         expect(arrSum(arr1 , arr2)).toBe(15);
     });
 
-    it("Should return the sum of the values ​​of arrays", () => {
+    it("Should return the sum of the values ​​of the two arrays", () => {
         const arr1 = [100, 200, 300];
         const arr2 = [400, 500, 600];
         expect(arrSum(arr1 , arr2)).toBe(2100);
     });
 }
 
-)
+);
